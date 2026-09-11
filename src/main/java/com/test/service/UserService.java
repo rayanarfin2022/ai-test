d4fb0e90794ab0525b1ff5a3ad6b2188542bb7a0
rayanarfin2022 <rayan.arfin2022@gmail.com>
2026-09-11 21:38:59 +0530

    Update UserService.java

--- src/main/java/com/test/service/UserService.java
+++ src/main/java/com/test/service/UserService.java
@@ -13,9 +13,7 @@ public class UserService {
     public UserService(UserRepository repo) {
         this.repo = repo;
     }
-      public String loadOrders(String userInput) {
-        return repo.runQuery(buildOrderQuery(userInput));
-    }
+    
     public String getUserByQuery(String query) {
         return repo.runQuery(query);
     }
